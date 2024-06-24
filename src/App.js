4c696286095f7b8d842e424f9ff0a127e45c4440
Александра <smisuk17@gmail.com>
2024-06-24 03:28:15 +0300

    all commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Head from "./components/head/head"
+import Home from "./components/articlecomps/home/home";
+import Contacts from "./components/articlecomps/contacts/contacts";
+import {Routes, Route} from 'react-router-dom';
+import Hiking from "./components/articlecomps/hiking/hiking";
+import Faq from "./components/articlecomps/faq/faq";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <Head/>
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/hiking" element={<Hiking/>}/>
+              <Route path="/faq" element={<Faq/>}/>
+              <Route path="/contacts" element={<Contacts/>}/>
+          </Routes>
+      </div>
   );
 }
-
-export default App;
